2eaa15e4800fb0d3ff535cb20a814d4628a729f7
kxtzownsu <kxtzownsu@gmail.com>
2025-05-13 19:08:01 +0200

    add commit hash & build time
    Signed-off-by: kxtzownsu <kxtzownsu@gmail.com>

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,8 @@
 import "../tailwind.css";
 
+const COMMIT_HASH = import.meta.env.VITE_COMMIT_HASH || "f00dcafe"; // iykyk :trolley:
+const BUILD_TIME = import.meta.env.VITE_BUILD_TIME || Math.floor(Date.now() / 1000);
+
 export function NavBar() {
   return (
     <header className="w-full h-16 text-xl bg-bg_secondary text-primary flex items-center font-Space">
@@ -13,13 +16,17 @@ export function NavBar() {
           style={{ boxShadow: "0 0 20px #cba6f7", borderRadius: "50%" }}
           alt="Logo"
         />
-        <h1 className="text-lg">kxtz' download server</h1>
+          <h1 className="text-lg">
+            kxtz' download server 
+            <br /> 
+            <sub className="text-xs text-muted">
+              {BUILD_TIME}-g{COMMIT_HASH}
+            </sub>
+          </h1>
       </div>
 
       {/* Middle Section of the Navbar */}
-      <div className="flex-grow text-center">
-        <p></p>
-      </div>
+      <div className="flex-grow text-center"></div>
 
       {/* Right Side of the Navbar */}
       <div className="mr-4"></div>
@@ -34,3 +41,4 @@ export function Footer() {
     </footer>
   );
 }
+

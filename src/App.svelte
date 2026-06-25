fe97c6c16327bbc38e046064f975ebea9639a825
kxtzownsu <kxtzownsu@gmail.com>
2026-06-25 02:00:41 -0400

    feat: add svelte foundation
    The new dl.kxtz.dev is going to be based
    on Svelte instead of Preact, although it still
    uses Tailwind CSS.
    
    Signed-off-by: kxtzownsu <kxtzownsu@gmail.com>

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Test from './components/test.svelte'
+</script>
+
+<main class="bg-base text-mauve min-w-screen min-h-screen max-w-screen max-h-screen">
+  <h1 class="text-2xl">dl.kxtz.dev</h1>
+  <Test />
+</main>

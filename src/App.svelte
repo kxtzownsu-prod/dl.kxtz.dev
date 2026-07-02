f8b9a0eb1aa5a908bda642ff9fb35e1a03dbd477
kxtzownsu <kxtzownsu@gmail.com>
2026-07-02 07:12:49 -0400

    feat: do some more theming
    Signed-off-by: kxtzownsu <kxtzownsu@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,20 @@
 <script>
   import './app.css';
-  
+
   import FileExplorer from './components/file-explorer/FileExplorer.svelte'
 </script>
 
-<main class="bg-background text-text font-normal min-w-screen min-h-screen max-w-screen max-h-screen">
-  <h1 class="text-xs">dl.kxtz.dev_{__BUILD_TIME__}-{__GIT_HASH_SHORT__}</h1>
-  <div class="font-terminal">
+<main class="bg-background text-primary font-normal min-w-screen min-h-screen max-w-screen max-h-screen p-8">
+  <div class="py-2">
+    <h1 class="text-sm text-secondary">dl.kxtz.dev_{__BUILD_TIME__}-{__GIT_HASH_SHORT__}</h1>
+  </div>
+  
+  <div class="font-terminal py-2">
     <!--
       This will eventually become breadcrumbs, so just wait
       until then I guess.
     -->
-    <div style="display: flex; font-size:1rem;">
+    <div class="flex py-2 text-md">
       <p class="file-list-entry">/</p>
       <p class="file-list-entry">Foobar</p>
       <p class="file-list-separator">/</p>

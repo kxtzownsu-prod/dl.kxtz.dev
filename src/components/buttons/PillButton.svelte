7a3809733407b452234957ffd11d19eaf50fe673
kxtzownsu <kxtzownsu@gmail.com>
2026-08-24 19:20:05 -0400

    feat: buttons: add new PillButton
    Signed-off-by: kxtzownsu <kxtzownsu@gmail.com>

---
+++ src/components/buttons/PillButton.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Icon from '../icon/Icon.svelte';
+
+  let { icon = "", text = "", children, ...rest } = $props();
+</script>
+
+<button
+  class="cursor-pointer p-2 bg-primary text-on_primary rounded-full inline-flex items-center gap-1"
+  {...rest}
+>
+  <Icon name={icon} />
+  {#if children}
+    {@render children()}
+  {:else}
+    {text}
+  {/if}
+</button>
